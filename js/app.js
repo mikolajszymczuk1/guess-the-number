5601f53dc6b25a78e76bbac931dc28cc02e7e7fd
Mikołaj Szymczuk <34286232+mikolajszymczuk1@users.noreply.github.com>
2019-10-22 19:23:14 +0200

    Project has ended !

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-console.log("Js is here :)");
+// Get game elements.
+const number_box = document.querySelector(".form__input");
+const clear_button = document.querySelector(".form__btn--clear");
+const check_button = document.querySelector(".form__btn--check");
+const output = document.querySelector(".game-box__output");
+const try_counter = document.querySelector(".game-box__try-counter");
+
+// Generate computer number and set try counter to 0.
+const computer_num = Math.floor(Math.random() * 101);
+let counter = 0;
+
+// Clear and check operations.
+clear_button.addEventListener("click", function () {
+    number_box.value = "";
+});
+
+// Game logic.
+check_button.addEventListener("click", function () {
+    output.classList.add("game-box__output--active");
+
+    if (number_box.value === "") {
+        output.innerHTML = "NIE PODANO LICZBY";
+    } else if (number_box.value < computer_num) {
+        output.innerHTML = "MOJA LICZBA JEST WIĘKSZA";
+        try_counter.innerHTML = String(++counter);
+    } else if (number_box.value > computer_num) {
+        output.innerHTML = "MOJA LICZBA JEST MNIEJSZA";
+        try_counter.innerHTML = String(++counter);
+    } else {
+        output.innerHTML = "ZGADŁEŚ TO MOJA LICZBA !";
+    }
+});
